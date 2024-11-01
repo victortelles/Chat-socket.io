868756b56a4ab68cc6acf8d4b23dcb5816d29050
VMLAP\VMLap <victor.telles@iteso.mx>
2024-10-31 21:33:51 -0600

    Moviendo index. dentro de src

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
 import express from 'express';
 import { config } from 'dotenv';
+import routes from './routes';
 //import socketIo from 'socket.io';
 import { Server } from 'socket.io';
+
 config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+// Middleware para parsear JSON
+app.use(express.json());
+
+//Importar rutas
+app.use(routes)
 
 //Importar el socket.io (server) (similar al "app.listen")
 const server = app.listen(port, () => {
@@ -20,5 +28,8 @@ io.on('connection', (socket) => {
     socket.on('joinRoom', (roomId) => {
         socket.join(`room-${roomId}`);
     });
+
+    //Importar la funcionalidad del controlador.
+    require('./controllers/chat.controller').sendMessage(io, socket);
 
 });
