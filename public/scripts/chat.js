20c36c2f148c4ddcf003a9d5ec4e4d400f1142f0
VMLAP\VMLap <victor.telles@iteso.mx>
2024-10-31 22:24:23 -0600

    Correccion de estilos y implementacion de logica para mostrar mensaje en el contenedor de div con DOM

--- public/scripts/chat.js
+++ public/scripts/chat.js
@@ -14,3 +14,41 @@ const roomId = window.location.href.split('/').pop();
 
 //Cliente se une a una sala especifica desde url.
 socket.emit('joinRoom', roomId);
+
+//Referencia a un ID usuario temporal
+const userId = `user-${Math.random().toString(36).substr(2, 9)}`;
+
+//Manejar el envio de forms de mensajes
+messageForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    //Hora
+    const timestamp = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: true });
+    const message = messageInput.value;
+
+    if (message.trim()) {
+        //Envia mensaje al servidor (sala y Id user.remitente)
+        socket.emit('sendNewMessage', { message, room: roomId, senderId: userId, timestamp });
+        console.log(`(ID: ${roomId}, Mensaje: ${message})`);
+        messageInput.value = '';    //Limpiar el campo despues de enviar
+    }
+});
+
+//Escuchar nuevos mensajes recibidos del servidor y mostrarlos en el chat
+socket.on('messageReceived', (data) => {
+    displayMessage(data, false);
+});
+
+// Función para mostrar mensajes en el chat con estilos
+function displayMessage(data, isOwnMessage) {
+    //Crea un nuevo elemento (div)
+    const newMessage = document.createElement('div');
+    newMessage.classList.add('message');
+    newMessage.textContent = data.message;
+
+    //Aplica la clase de estilo segun el remitente.
+    newMessage.classList.add(isOwnMessage || data.senderId === userId ? 'sent' : 'received');
+
+    //Agregar el mensaje al contenedor.
+    messagesContainer.appendChild(newMessage);
+}
