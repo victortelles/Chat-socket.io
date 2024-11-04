6a9c7c729dd43f42a6cd129a9d68e896dd46250c
VMLAP\VMLap <victor.telles@iteso.mx>
2024-11-04 12:14:32 -0600

    Creacion de intento de middleware

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,5 +1,14 @@
 import { Request, Response, NextFunction } from "express";
 
 export const validateUsername = (req: Request, res: Response, next: NextFunction) => {
+    const username = req.headers['username'] as string || req.body.username;
 
+    if (username) {
+        console.log(`Middleware: Usuario ${username} autenticado`);
+        req.body.username = username;
+        next();
+    } else {
+        console.log(`Middleware: ${username} No se recibió el username en la solicitud`);
+        res.status(400).json({ message: 'Username is required' });
+    }
 }
